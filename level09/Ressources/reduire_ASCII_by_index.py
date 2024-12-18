4c48e7182e71642e620985b1ebe17444aa251443
Charles Mariot <contact@charles-mariot.fr>
2024-12-18 18:28:35 +0100

    Update reduire_ASCII_by_index.py correction com

--- level09/Ressources/reduire_ASCII_by_index.py
+++ level09/Ressources/reduire_ASCII_by_index.py
@@ -1,12 +1,12 @@
-import sys  # Importation du module sys pour accéder aux arguments de la ligne de commande
-# Récupération de l'argument passé en ligne de commande (le hash)
-chaine_hachée = sys.argv[1]
-# Initialisation d'une variable pour stocker le résultat final
+import sys  # Importation du module sys pour acceder aux arguments de la ligne de commande
+# Recuperation de l'argument passed en ligne de commande (le hash)
+chaine_hachee = sys.argv[1]
+# Initialisation d'une variable pour stocker le resultat final
 message_original = ""
-# Parcours de chaque caractère dans la chaîne hachée
-for indice in range(0, len(chaine_hachée)):
-    # Pour chaque caractère, on modifie son code ASCII en fonction de sa position (indice)
-    # On ajoute le caractère modifié à la chaîne 'message_original'
-    message_original = message_original + chr(ord(chaine_hachée[indice]) - indice)
-# Affichage du message décodé
+# Parcours de chaque caractere dans la chaine hachee
+for indice in range(0, len(chaine_hachee)):
+    # Pour chaque caractere, on modifie son code ASCII en fonction de sa position (indice)
+    # On ajoute le caractere modifie a la chaine 'message_original'
+    message_original = message_original + chr(ord(chaine_hachee[indice]) - indice)
+# Affichage du message decode
 print(message_original)
