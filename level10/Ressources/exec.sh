4f5eeb7d03231182b25063f0921c43c8a903fee4
Charles Mariot (42) <contact@charles-mariot.fr>
2025-01-02 13:42:14 +0100

    Level10 : suppression des commentaires accentues et ajout d'exec.sh

---
+++ level10/Ressources/exec.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+TMP_FILE="/tmp/tmpfile"
+
+while true
+    do
+        ./level10 $TMP_FILE 127.0.0.1
+done
--- level10/Ressources/tcp_server.py
+++ level10/Ressources/tcp_server.py
@@ -1,23 +1,24 @@
-import socket  # Import du module socket
+import socket
 
-PORT = 6969  # Port d'écoute
-HOST = '0.0.0.0'  # Écoute sur toutes les interfaces
+PORT = 6969
+HOST = '0.0.0.0'
+
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Création du socket TCP
-    s.bind((HOST, PORT))  # Liaison à l'adresse et au port
-    s.listen(1)  # Écoute d'une connexion
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
 
     while True:
-        conn, addr = s.accept()  # Accepte la connexion d'un client
-        print('Connected by', addr)  # Affiche l'adresse du client
-        
-        while True:
-            data = conn.recv(1024)  # Reçoit des données
-            if not data:  # Si aucune donnée, déconnecte
-                break
-            print(data)  # Affiche les données reçues
+        conn, addr = s.accept()
+        print('Connected by', addr)
 
-# Exécution uniquement si le script est lancé directement
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data)
+
+
 if __name__ == '__main__':
-    main()  # Démarre le serveur
+    main()
